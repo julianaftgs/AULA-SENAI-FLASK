fc6e1e36da5f784c34df87b20ec2af7033f804c3
julianaftgs <julianaftgs@gmail.com>
2025-10-07 21:39:37 -0300

    quinta

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Ola GitHub")
+from flask import Flask, request
+
+APP = Flask(__name__)
+
+#endpoint: buscar todos os alunos
+@app.route('buscaralunos', methods=['GET'])
+def buscar_alunos():
+    return "todos alunos listados"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
